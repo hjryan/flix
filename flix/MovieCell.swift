6a3f9287da6d0b95ddfd9eea639aa399b166bd36
Hannah Ryan <hjryan@uw.edu>
2020-10-02 20:01:21 -0700

    + custom cell, movie poster

---
+++ flix/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by hannah ryan on 10/2/20.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    // Create outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
