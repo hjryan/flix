ef847468143475f8cea2a5c428944ae11351cf87
Hannah Ryan <hjryan@uw.edu>
2020-10-04 13:56:59 -0700

    + detail view from collection view, changed collection view from superhero to old movies

--- flix/MovieGridViewController.swift
+++ flix/MovieGridViewController.swift
@@ -37,7 +37,7 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
 
         // Do any additional setup after loading the view.
         // Call API -- get movies
-        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&page=1")!
+        let url = URL(string: "https://api.themoviedb.org/3/movie/105/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
@@ -75,14 +75,25 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         return cell
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        // Find the selected movie
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)!
+        let movie = movies [indexPath.row]
+        
+        // Pass the selected movie to the movie details view controller
+        let detailsViewController = segue.destination as! MovieGridDetailsViewController
+        detailsViewController.movie = movie
+        
+       
     }
-    */
+    
 
 }
